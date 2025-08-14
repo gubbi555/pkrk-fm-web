d3a44e4d070e154d55262b375a8958ade7b0d6a7
gubbi555 <105633204+gubbi555@users.noreply.github.com>
2025-08-14 22:23:16 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContentBrowser from './components/ContentBrowser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ContentBrowser />
+    </div>
+  );
+}
+
+export default App;
