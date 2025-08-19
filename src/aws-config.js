724d0a30d7ef5db58e7e3d9d8d281c369344109d
Prakash K. <prakash.k@ittstarglobal.com>
2025-08-19 18:07:23 +0530

    Fix Cognito client secret issue - use client without secret

--- src/aws-config.js
+++ src/aws-config.js
@@ -4,7 +4,7 @@ const awsconfig = {
   aws_project_region: 'ap-south-1',
   aws_cognito_region: 'ap-south-1',
   aws_user_pools_id: 'ap-south-1_x76asAEFZ',
-  aws_user_pools_web_client_id: '631msbrfpevca0h86hvoemim1j',
+  aws_user_pools_web_client_id: '631msbrfpevca0h86hvoemim1j', // Replace with new client ID
   aws_cognito_username_attributes: ['EMAIL'],
   aws_cognito_social_providers: [],
   aws_cognito_signup_attributes: ['EMAIL'],
