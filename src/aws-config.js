16ca57c4b9a268ff32279656de2ba64b8015d2a9
Prakash K. <prakash.k@ittstarglobal.com>
2025-08-19 16:16:23 +0530

    new commits

--- src/aws-config.js
+++ src/aws-config.js
@@ -4,8 +4,8 @@ const awsconfig = {
   Auth: {
     Cognito: {
       region: 'ap-south-1',
-      userPoolId: 'ap-south-1_XXXXXXXXX', // Replace with your User Pool ID
-      userPoolClientId: 'abcd1234efgh5678ijkl', // Replace with your App Client ID
+      userPoolId: 'ap-south-1_x76asAEFZ', // Replace with your User Pool ID
+      userPoolClientId: '631msbrfpevca0h86hvoemim1j', // Replace with your App Client ID
       signUpVerificationMethod: 'code',
       loginWith: {
         email: true,
