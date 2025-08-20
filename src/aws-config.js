9f6cae83541cbf8260b6c41a8f4b9f6f85dfded5
Prakash K. <prakash.k@ittstarglobal.com>
2025-08-20 20:19:19 +0530

    Fix Amplify deployment - update build commands and  p

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,15 +1,13 @@
 import { Amplify } from 'aws-amplify';
 
 const awsconfig = {
-  aws_project_region: 'ap-south-1',
-  aws_cognito_region: 'ap-south-1',
-  aws_user_pools_id: 'ap-south-1_kajtyBPTH', // From new pool
-  aws_user_pools_web_client_id: '11bsmqndbd8dj7k58hs7u6at2a', // From new SPA client
-  aws_cognito_username_attributes: ['EMAIL'],
-  aws_cognito_social_providers: [],
-  aws_cognito_signup_attributes: ['EMAIL'],
-  aws_cognito_mfa_configuration: 'OFF',
-  aws_cognito_verification_mechanisms: ['EMAIL']
+  Auth: {
+    region: 'ap-south-1',
+    userPoolId: 'ap-south-1_kajtyBPTH',
+    userPoolWebClientId: '11bsmqndbd8dj7k58hs7u6at2a',
+    mandatorySignIn: false,
+    authenticationFlowType: 'USER_SRP_AUTH'
+  }
 };
 
 Amplify.configure(awsconfig);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './aws-config'; // <-- This line must come BEFORE any Auth usage
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
